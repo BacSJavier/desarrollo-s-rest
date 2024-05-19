2bcb2db04b4d1295283a218bece62050289687ae
Javier Bacilio Sanchez <l21140750@queretaro.tecnm.mx>
2024-05-18 21:10:45 -0600

    setup

---
+++ src/main/java/com/example/proyectodesarrollosrest/ProyectoDesarrolloSRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectodesarrollosrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDesarrolloSRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDesarrolloSRestApplication.class, args);
+	}
+
+}
